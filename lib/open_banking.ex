013f02695a44ee2208da6bc8ddddb6904d818378
robmckinnon <rob@movingflow>
2018-09-09 16:17:17 +0100

    Add client credentials grant request access token fn
    Use Joken.Signer to sign claims.
    
    Pass transport key/cert file names as parameters.

--- lib/open_banking.ex
+++ lib/open_banking.ex
@@ -2,4 +2,16 @@ defmodule OpenBanking do
   @moduledoc """
   For performing calls against Open Banking API.
   """
+
+  defdelegate client_credentials_grant_request_access_token(
+                client_id,
+                token_endpoint,
+                signing_key,
+                token_endpoint_auth_method \\ "private_key_jwt",
+                scope \\ "accounts payments",
+                transport_key_file \\ "./certificates/transport.key",
+                transport_cert_file \\ "./certificates/transport.pem"
+              ),
+              to: OpenBanking.ClientCredentialsGrant,
+              as: :request_access_token
 end
